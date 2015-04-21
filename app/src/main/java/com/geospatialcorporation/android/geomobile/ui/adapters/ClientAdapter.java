590ba1b9a293bbdf5c9226365c944a90c5650e71
andreij6 <andreij6@gmail.com>
2015-04-21 11:45:04 -0400

    All of the changes. All of the things.

--- app/src/main/java/com/geospatialcorporation/android/geomobile/ui/adapters/ClientAdapter.java
+++ app/src/main/java/com/geospatialcorporation/android/geomobile/ui/adapters/ClientAdapter.java
@@ -1,6 +1,7 @@
 package com.geospatialcorporation.android.geomobile.ui.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -13,6 +14,7 @@ import com.geospatialcorporation.android.geomobile.R;
 import com.geospatialcorporation.android.geomobile.application;
 import com.geospatialcorporation.android.geomobile.library.rest.LoginService;
 import com.geospatialcorporation.android.geomobile.models.Client;
+import com.geospatialcorporation.android.geomobile.ui.MainActivity;
 
 import java.util.List;
 
@@ -22,15 +24,13 @@ public class ClientAdapter extends RecyclerView.Adapter<ClientAdapter.ClientView
     private static final String TAG = "ClientAdapter";
     private Context mContext;
     private List<Client> mClients;
-    private LoginService service;
     private int newClientId;
+    private LoginService mService;
 
     public ClientAdapter(Context context, List<Client> clients){
         mContext = context;
         mClients = clients;
-
-        service = application.getRestAdapter().create(LoginService.class);
-        getClients();
+        mService = application.getRestAdapter().create(LoginService.class);
     }
 
     @Override
@@ -63,6 +63,17 @@ public class ClientAdapter extends RecyclerView.Adapter<ClientAdapter.ClientView
         public ClientViewHolder(View itemView){
             super(itemView);
             mClientName = (TextView)itemView.findViewById(R.id.clientNameLabel);
+
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    newClientId = mClient.Id;
+
+                    new SwitchClientTask().execute();
+
+                    mContext.startActivity(new Intent(mContext, MainActivity.class));
+                }
+            });
         }
 
         public void bindClientItem(Client client){
@@ -70,52 +81,27 @@ public class ClientAdapter extends RecyclerView.Adapter<ClientAdapter.ClientView
             mClientName.setText(client.Name);
         }
 
-        protected View.OnClickListener ClientItemClicked = new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-              newClientId = mClient.Id;
-
-                switchClient();
-            }
-        };
     }
 
-    void switchClient() {
-        AsyncTask task = new AsyncTask() {
-            @Override
-            protected List<Client> doInBackground(Object[] params) {
-                try {
-                    service.setClient(newClientId);
-                } catch (RetrofitError e) {
-                    if (e.getResponse() != null) {
-                        Log.d(TAG, Integer.toString(e.getResponse().getStatus()));
-                    }
+
+
+    private class SwitchClientTask extends AsyncTask{
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            try {
+                mService.setClient(newClientId);
+
+            } catch (RetrofitError e) {
+                if (e.getResponse() != null) {
+                    Log.d(TAG, Integer.toString(e.getResponse().getStatus()));
                 }
-
-                return mClients;
             }
-        };
 
-        task.execute();
+            return null;
+        }
     }
 
-    void getClients() {
-        AsyncTask task = new AsyncTask() {
-            @Override
-            protected List<Client> doInBackground(Object[] params) {
-                try {
-                    mClients = service.getClients();
-                } catch (RetrofitError e) {
-                    if (e.getResponse() != null) {
-                        Log.d(TAG, Integer.toString(e.getResponse().getStatus()));
-                    }
-                }
 
-                return mClients;
-            }
-        };
 
-        task.execute();
-    }
 }
--- app/src/main/java/com/geospatialcorporation/android/geomobile/ui/fragments/ClientFragment.java
+++ app/src/main/java/com/geospatialcorporation/android/geomobile/ui/fragments/ClientFragment.java
@@ -1,26 +1,33 @@
 package com.geospatialcorporation.android.geomobile.ui.fragments;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.geospatialcorporation.android.geomobile.R;
+import com.geospatialcorporation.android.geomobile.application;
+import com.geospatialcorporation.android.geomobile.library.rest.LoginService;
 import com.geospatialcorporation.android.geomobile.ui.adapters.ClientAdapter;
 import com.geospatialcorporation.android.geomobile.models.Client;
-import com.google.gson.Gson;
 
 import java.util.List;
 
+import retrofit.RetrofitError;
+
 public class ClientFragment extends Fragment {
+
     private static final String TAG = "ClientFragment";
+
     private RecyclerView mRecyclerView;
     private List<Client> mDataSet;
     private View mRootView;
-    private Gson gson = new Gson();
+    private LoginService service;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -29,14 +36,37 @@ public class ClientFragment extends Fragment {
 
         mRecyclerView = (RecyclerView)mRootView.findViewById(R.id.clientitem_recyclerView);
 
-        ClientAdapter adapter = new ClientAdapter(getActivity(), mDataSet);
+        service = application.getRestAdapter().create(LoginService.class);
 
-        mRecyclerView.setAdapter(adapter);
+        new GetClientsTask().execute();
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(container.getContext());
 
         mRecyclerView.setLayoutManager(layoutManager);
 
         return mRootView;
+    }
+
+    private class GetClientsTask extends AsyncTask<Void, Void, List<Client>> {
+        @Override
+        protected List<Client> doInBackground(Void... params) {
+            try {
+                mDataSet = service.getClients();
+            } catch (RetrofitError e) {
+                if (e.getResponse() != null) {
+                    Log.d(TAG, e.getResponse().getStatus() + " : Line 112");
+                }
+            }
+
+            return mDataSet;
+        }
+
+        @Override
+        protected void onPostExecute(List<Client> clients){
+            ClientAdapter adapter = new ClientAdapter(getActivity(), mDataSet);
+
+            mRecyclerView.setAdapter(adapter);
+
+        }
     }
 }
