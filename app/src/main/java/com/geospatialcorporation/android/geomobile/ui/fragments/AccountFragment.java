b2f205c1c7242a9eb43296d05090067863828efc
andreij6 <andreij6@gmail.com>
2015-04-23 18:14:19 -0400

    Account View Setup

--- app/src/main/java/com/geospatialcorporation/android/geomobile/ui/fragments/AccountFragment.java
+++ app/src/main/java/com/geospatialcorporation/android/geomobile/ui/fragments/AccountFragment.java
@@ -1,6 +1,5 @@
 package com.geospatialcorporation.android.geomobile.ui.fragments;
 
-import android.accounts.Account;
 import android.app.Fragment;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -8,16 +7,19 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.geospatialcorporation.android.geomobile.R;
 import com.geospatialcorporation.android.geomobile.application;
-import com.geospatialcorporation.android.geomobile.library.rest.LoginService;
-import com.geospatialcorporation.android.geomobile.models.Client;
+import com.geospatialcorporation.android.geomobile.library.rest.AccountService;
+import com.geospatialcorporation.android.geomobile.models.UserAccount;
 
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 import retrofit.RetrofitError;
 
 public class AccountFragment extends Fragment {
@@ -26,21 +28,23 @@ public class AccountFragment extends Fragment {
 
     //region Properties
     View mRootView;
-    Client mClient;
-    LoginService mService;
+    UserAccount mUserAccount;
+    AccountService mService;
     //endregion
 
     //region View Setup
-    @InjectView(R.id.nameTV) TextView NameTV;
-    @InjectView(R.id.emailTV) TextView EmailTV;
-    @InjectView(R.id.phoneTV) TextView PhoneTV;
+    @InjectView(R.id.firstNameET) EditText FirstName;
+    @InjectView(R.id.lastNameEt) EditText LastName;
+    @InjectView(R.id.emailET) EditText Email;
+    @InjectView(R.id.cellPhoneET) EditText CellPhone;
+    @InjectView(R.id.officePhoneET) EditText OfficePhone;
     //endregion
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         mRootView = inflater.inflate(R.layout.fragment_account, container, false);
         ButterKnife.inject(this, mRootView);
 
-        mService = application.getRestAdapter().create(LoginService.class);
+        mService = application.getRestAdapter().create(AccountService.class);
 
         new GetProfileTask().execute();
 
@@ -48,26 +52,35 @@ public class AccountFragment extends Fragment {
     }
 
     private void SetupUI() {
-        NameTV.setText(mClient.Name);
+        FirstName.setText(mUserAccount.getFirstName());
+        LastName.setText(mUserAccount.getLastName());
+        Email.setText(mUserAccount.getEmail());
+        CellPhone.setText(mUserAccount.getCellPhone());
+        OfficePhone.setText(mUserAccount.getOfficePhone());
     }
 
-    private class GetProfileTask extends AsyncTask<Void, Void, Client>{
+    @OnClick(R.id.saveAcctBtn)
+    public void save(){
+        Toast.makeText(getActivity(), "Your Account changes have NOT Been saved!!", Toast.LENGTH_LONG).show();
+    }
+
+    private class GetProfileTask extends AsyncTask<Void, Void, UserAccount>{
 
         @Override
-        protected Client doInBackground(Void... params) {
+        protected UserAccount doInBackground(Void... params) {
             try {
-                mClient = mService.getCurrentClient();
+                mUserAccount = mService.getProfile();
             } catch (RetrofitError e) {
                 if (e.getResponse() != null) {
                     Log.d(TAG, e.getResponse().getStatus() + " : Line 70");
                 }
             }
 
-            return mClient;
+            return mUserAccount;
         }
 
         @Override
-        protected void onPostExecute(Client client){
+        protected void onPostExecute(UserAccount account){
             SetupUI();
         }
     }
